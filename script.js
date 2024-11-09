3f7715813d6624bb69258cbe4924299f6e1888c9
Akila-Rahman <73312109+Akila-Rahman@users.noreply.github.com>
2024-11-10 00:29:23 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function addToCart(item) {
     alert(item + " has been added to your cart!");
 }
+function myFunction(images) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
